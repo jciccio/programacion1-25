82bcdae2ec4c3d86134ba006afe0e3c7f119a447
Jose Antonio <jciccio@gmail.com>
2025-04-03 17:41:27 -0600

    Ejemplo primo

--- Matematicas.java
+++ Matematicas.java
@@ -40,11 +40,41 @@ public class Matematicas{
 		return resultado;
 	}
 
+	// Calcular si un numero es primo
+	// Se puede dividir entre 1 y si mismo nada mas
+	// El primer primo es 2
+	// 1.000.000 -> 500.000
+	public boolean esPrimo(int n){
+		boolean primo = true;
+		int contador = 2;
+		if(n <= 1){
+			primo = false;
+		}
+		while(contador <= n/2 && primo){
+			if(n%contador == 0){
+				primo = false;
+			}
+			contador++;
+		}
+		return primo;
+	}
+
 	public static void main (String [] args){
 		Matematicas mate = new Matematicas();
 		System.out.println(mate.calcularFactorial(4));
 		System.out.println(mate.calcularPotencia(2,8));
 		System.out.println(mate.calcularCoseno(4,8));
+
+		System.out.println(mate.esPrimo(2)); // True
+		System.out.println(mate.esPrimo(10)); // False
+		System.out.println(mate.esPrimo(1000000000)); // False
+		System.out.println(mate.esPrimo(1000000000)); // False
+		System.out.println(mate.esPrimo(1000000000)); // False
+		System.out.println(mate.esPrimo(1000000000)); // False
+		System.out.println(mate.esPrimo(1000000000)); // False
+		System.out.println(mate.esPrimo(1000000000)); // False
+		System.out.println(mate.esPrimo(121)); // False
+		System.out.println(mate.esPrimo(13));
 	}
 
 }
