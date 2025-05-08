c31340ccdf5342f1c5f06cec37171a44044e05c2
Jose Antonio <jciccio@gmail.com>
2025-05-08 17:35:34 -0600

    Ejemplo buscaminas y multiplicacion de matrices

--- Buscaminas.java
+++ Buscaminas.java
@@ -1,3 +1,82 @@
 public class Buscaminas{
-	
+	// Matriz de numeros donde el -1 es una mina
+	// Matriz de despligue => M _ 
+	public int [][] tablero;
+	public String [][] tableroUsuario;
+	public int minas;
+
+	// Constructor que inicialice las minas y el tablero	
+	public Buscaminas(int f, int c, int minas){
+		if(f*c > minas){
+			tablero = new int[f][c];
+			tableroUsuario = new String[f][c];
+			this.minas = minas;
+			llenarMinas();
+		}
+	}
+
+
+	// Llenar el tablero de minas
+	public void llenarMinas(){
+		int minasRestantes = minas;
+		while(minasRestantes > 0){
+			// Dos numeros aleatorios uno por fila y uno por col Math.random() //[0,1[
+			// Tam filas y columnas
+			int filaAleatoria = (int)(Math.random() * tablero.length);
+			int colAleatoria =  (int)(Math.random() * tablero[0].length);
+			if(tablero[filaAleatoria][colAleatoria] != -1){
+				tablero[filaAleatoria][colAleatoria] = -1;
+				minasRestantes--;
+				llenarNumeros(filaAleatoria, colAleatoria);
+			}
+		}
+	} 
+
+
+	// Llenar los números del tablero 
+	public void llenarNumeros(int filaMina, int columnaMina){
+		// Chequear los limites antes de acceder a la celda
+		int [] cF = {-1, -1, -1, 0, 0, 1, 1, 1};
+		int [] cC = {-1,  0,  1,-1, 1,-1, 0, 1};
+		for(int i = 0; i < cF.length; i++){
+			int nuevaFila = filaMina    + cF[i];
+			int nuevaCol  = columnaMina + cC[i];
+			if(nuevaFila >= 0 && nuevaFila < tablero.length && 
+			   nuevaCol >= 0 && nuevaCol < tablero[0].length &&
+			   tablero[nuevaFila][nuevaCol] != -1 ){
+
+				tablero[nuevaFila][nuevaCol]++;
+
+			}
+		}
+	}
+
+
+	public String toString(){
+		String tableroString = "Minas: " + minas + "\n" ;
+
+		String tableroUsuarioString ="";
+		for(int f = 0 ; f < tablero.length; f++){
+			for(int c = 0; c < tablero[f].length; c++){
+				tableroString += tablero[f][c] + "\t";
+				if(tableroUsuario[f][c] == null){
+					tableroUsuarioString += "_\t"; 
+				}
+				else{
+					tableroUsuarioString += tableroUsuarioString[f][c];	
+				}
+				
+			}
+			tableroUsuarioString +="\n";
+			tableroString += "\n";
+		}
+		return tableroString + tableroUsuarioString;
+	}
+
+
+	public static void main (String args[]){
+		Buscaminas buscaminas = new Buscaminas(10,10,20);
+		System.out.println(buscaminas); 
+	}
+
 }
--- Matrices.java
+++ Matrices.java
@@ -73,8 +73,44 @@ public class Matrices{
 		}
 	}
 
+
+	public int [][] multiplicarMatrices (int [][] matrizA, int[][] matrizB){
+		// Ver si se puede multiplicar 
+		// SI la cantidad de columnas de la MatrizA es = a la cantidad e filas de la matrizB
+		int [][] resultado = null;
+		if(matrizA[0].length == matrizB.length){
+			// Se puede multiplicar
+			resultado = new int [matrizA.length][matrizB[0].length];
+			for(int f = 0; f < resultado.length; f++){
+				for(int c =0; c < resultado[f].length; c++){
+					resultado[f][c] = calcularProductoPunto(matrizA[f], obtenerColumna(matrizB, c));
+				}
+			}
+		}
+		return resultado;
+	}
+
+
+	public int [] obtenerColumna (int [][] matriz, int columna){
+		int [] resultado = new int [matriz.length];
+		for(int i =0 ; i < matriz.length; i++){
+			resultado[i] = matriz[i][columna];
+		}
+		return resultado;
+	}
+
+	public int calcularProductoPunto(int[] a, int []b){
+		int resultado = 0;
+		for(int i = 0; i < a.length; i++){
+			resultado += a[i] * b[i];
+		}
+
+		return resultado;
+	}
+
+
 	public static void main (String [] args){
-		int [][] matrizNumeros = new int [5][10]; 
+		/*int [][] matrizNumeros = new int [5][10]; 
 
 		matrizNumeros[2][1] = 6;
 		matrizNumeros[3][1] = 61;
@@ -91,7 +127,9 @@ public class Matrices{
 		System.out.println((int)'B');
 		System.out.println((int)'Z');
 
-		matrizInstancia.imprimir(matrizInstancia.crearMatrizLetras(20,3));
+		matrizInstancia.imprimir(matrizInstancia.crearMatrizLetras(20,3));*/
+
+
 		
 	}
 }
