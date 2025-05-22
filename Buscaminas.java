dc88df28e36d8873cd86730479f5121c9760ca76
Jose Antonio <jciccio@gmail.com>
2025-05-22 17:57:05 -0600

    Buscaminas para tarea

--- Buscaminas.java
+++ Buscaminas.java
@@ -1,82 +1,88 @@
 public class Buscaminas{
-	// Matriz de numeros donde el -1 es una mina
-	// Matriz de despligue => M _ 
-	public int [][] tablero;
-	public String [][] tableroUsuario;
-	public int minas;
+	private int[][] tablero;
+	private String [][] tableroUsuario;
+	private int minas;
+	//-1 => Representa una mina
+	// 0-8 => Cantidad de minas alrededor
 
-	// Constructor que inicialice las minas y el tablero	
 	public Buscaminas(int f, int c, int minas){
 		if(f*c > minas){
-			tablero = new int[f][c];
-			tableroUsuario = new String[f][c];
 			this.minas = minas;
-			llenarMinas();
+			tablero = new int[f][c];
+			tableroUsuario = new String [f][c];		
+			for(int fila = 0 ; fila < tablero.length; fila++){
+				for(int col = 0; col <tablero[fila].length; col++){
+					tableroUsuario[fila][col] = "_";
+				}
+			}
+
+			while(minas > 0){
+				int filaAleatoria = (int) (Math.random()*f);
+				int columnaAleatoria = (int) (Math.random()*c);
+				if(tablero[filaAleatoria][columnaAleatoria] != -1){
+					tablero[filaAleatoria][columnaAleatoria] = -1;
+					minas--;
+				}
+			}
+			llenarTablero();
 		}
 	}
 
+	// Crear este método como tarea/trabajo en clase
+	//public /*tipoRetorno*/ realizarMovimiento(/*agregue aqui sus parametros*/){
+	//}
 
-	// Llenar el tablero de minas
-	public void llenarMinas(){
-		int minasRestantes = minas;
-		while(minasRestantes > 0){
-			// Dos numeros aleatorios uno por fila y uno por col Math.random() //[0,1[
-			// Tam filas y columnas
-			int filaAleatoria = (int)(Math.random() * tablero.length);
-			int colAleatoria =  (int)(Math.random() * tablero[0].length);
-			if(tablero[filaAleatoria][colAleatoria] != -1){
-				tablero[filaAleatoria][colAleatoria] = -1;
-				minasRestantes--;
-				llenarNumeros(filaAleatoria, colAleatoria);
-			}
-		}
-	} 
-
-
-	// Llenar los números del tablero 
-	public void llenarNumeros(int filaMina, int columnaMina){
-		// Chequear los limites antes de acceder a la celda
-		int [] cF = {-1, -1, -1, 0, 0, 1, 1, 1};
-		int [] cC = {-1,  0,  1,-1, 1,-1, 0, 1};
-		for(int i = 0; i < cF.length; i++){
-			int nuevaFila = filaMina    + cF[i];
-			int nuevaCol  = columnaMina + cC[i];
-			if(nuevaFila >= 0 && nuevaFila < tablero.length && 
-			   nuevaCol >= 0 && nuevaCol < tablero[0].length &&
-			   tablero[nuevaFila][nuevaCol] != -1 ){
-
-				tablero[nuevaFila][nuevaCol]++;
-
+	public void llenarTablero(){
+		int [] cF = {-1,-1,-1,0,0,1,1,1};
+		int [] cC = {-1,0,1,-1,1,-1,0,1};
+		for(int f = 0 ; f < tablero.length; f++){
+			for(int c = 0; c <tablero[f].length; c++){
+				if(tablero[f][c] == -1){
+					for(int i = 0; i < 8; i++){
+						int nuevaF = f + cF[i];
+						int nuevaC = c + cC[i];
+						if(nuevaF >= 0 && nuevaF < tablero.length &&
+						nuevaC >=0 && nuevaC  < tablero[nuevaF].length &&
+						tablero[nuevaF][nuevaC] != -1){
+							tablero[nuevaF][nuevaC]++;
+						}
+					}
+				}
 			}
 		}
 	}
 
+	
 
 	public String toString(){
-		String tableroString = "Minas: " + minas + "\n" ;
-
-		String tableroUsuarioString ="";
-		for(int f = 0 ; f < tablero.length; f++){
+		String contenido = "";
+		for(int f = 0; f < tablero.length; f++){
 			for(int c = 0; c < tablero[f].length; c++){
-				tableroString += tablero[f][c] + "\t";
-				if(tableroUsuario[f][c] == null){
-					tableroUsuarioString += "_\t"; 
-				}
-				else{
-					tableroUsuarioString += tableroUsuarioString[f][c];	
-				}
-				
+				contenido += tablero[f][c] ;
+				contenido += " \t";
 			}
-			tableroUsuarioString +="\n";
-			tableroString += "\n";
+			contenido += "\r\n";
 		}
-		return tableroString + tableroUsuarioString;
+		contenido += "\r\n - - -  Tablero Usuario - - - - - \r\n";
+		for(int fila = 0 ; fila < tableroUsuario.length; fila++){
+			for(int col = 0; col <tableroUsuario[fila].length; col++){
+				contenido += tableroUsuario[fila][col] + " \t";
+			}
+			contenido += "\r\n";
+		}
+		return contenido;
 	}
 
+	public void jugar(){
+		
+	}
 
-	public static void main (String args[]){
-		Buscaminas buscaminas = new Buscaminas(10,10,20);
-		System.out.println(buscaminas); 
+	public static void main (String [] args){
+		Buscaminas buscaminas = new Buscaminas(10,10,10);
+		System.out.println(buscaminas);
+		buscaminas.llenarTablero();
+		
+
 	}
 
 }
